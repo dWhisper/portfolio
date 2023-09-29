9ddb4244ce20dda8b391baa6a541b13565267cef
Nick Martin <dWhisper@users.noreply.github.com>
2023-09-29 10:38:07 -0500

    Trying to add one icon on windows
    It did not go well

---
+++ projects/core/src/icon/wrapped-icons/generic/Infinity.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { StyledSvg } from "../../Icon.styled";
+import { StyledSvgProps } from "../../Icon.types";
+
+export const Infinity = ({
+  size,
+  color,
+  pointerEvents,
+  ...rest
+}: StyledSvgProps) => {
+  const events = pointerEvents ? "auto" : "none";
+  return (
+    <StyledSvg
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      pointerEvents={events}
+      height={size}
+      fill={"none"}
+      width={size}
+      {...rest}
+    ><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5.63636 16C2.90909 16 2 14 2 12C2 10 2.90909 8 5.63636 8C9.27273 8 14.7273 16 18.3636 16C21.0909 16 22 14 22 12C22 10 21.0909 8 18.3636 8C14.7273 8 9.27273 16 5.63636 16Z" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </StyledSvg>
+  );
+};
